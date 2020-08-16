c399d8f42cacdf286eaaa355521cdaf488a3d2d5
Lee_JunWon <cpprhtn@naver.com>
2020-08-16 16:51:02 +0900

    8/16

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 
-class Neuron()
+#define MAX2(a,b) (a) > (b) ? (a) : (b)
+
+class Neuron{
+public:
+    double w_; //weight
+    double b_; //bias
+
+    double getAct(const double& x){
+        //for linear or identity activation functions
+        return x;
+
+        //for ReLU activation functions
+        //return MAX2(0.0)
+    }
+
+    double feedForward(const double& input){
+        //output y = f(\sigma)
+        //\sigma = w_ * input x + b
+        //for multiple inputs,
+        //\sigma = w0_ * x0_ + w1_ * x1_ ... + b
+        const double sigma = w_ * input + b_;
+
+        return getAct(sigma);
+    }
+};
 
 int main(){
-    std::cout << "Hello world" <<std::endl;
+
+    Neuron my_neuron;
+    my_neuron.w_ = 2.0;
+    my_neuron.b_ = 1.0;
+
+    std::cout << "Input 1.0" << my_neuron.feedForward(1.0) << std::endl;
+    std::cout << "Input 2.0" << my_neuron.feedForward(2.0) << std::endl;
+    std::cout << "Input 3.0" << my_neuron.feedForward(3.0) << std::endl;
+    
 }
