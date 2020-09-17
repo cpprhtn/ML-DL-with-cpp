080f6514afe17a5db642114153960bb375cf0f33
Lee_JunWon <cpprhtn@naver.com>
2020-09-17 22:29:44 +0900

    9/17

--- Maek_ML_Models/NaiveBayes_Model/naive_bayes.cpp
+++ Maek_ML_Models/NaiveBayes_Model/naive_bayes.cpp
@@ -97,11 +97,62 @@ NaiveBayesClassifier::NaiveBayesClassifier(
 
 	in.close();
 
-	// populate prob_word
+	//populate prob_word
 	for (ll i = 0; i < voca_size; i++) {
 		prob_word[i].first.first = (1.0 + freq_word[i].first.first) / (p_wobin_freq + voca_size);
 		prob_word[i].first.second = (1.0 + freq_word[i].first.second) / (n_wobin_freq + voca_size);
 		prob_word[i].second.first = (1.0 + freq_word[i].second.first) / (p_wbin_freq + voca_size);
 		prob_word[i].second.second = (1.0 + freq_word[i].second.second) / (n_wbin_freq + voca_size);
 	}
+}
+
+void NaiveBayesClassifier::test(const string& test_bow_file, bool use_bin) {
+	ifstream in(test_bow_file);
+	if (!in.is_open()) {
+		cerr << "File opening failed\n";
+		exit(0);
+	}
+
+	ll tp = 0, fp = 0, fn = 0, tn = 0;
+	string line;
+	//consider one review in each iteration
+	while (getline(in, line)) {
+		//obtain the sentiment of the review
+		ll rating;
+		stringstream ss;
+		ss.str(line);
+		ss >> rating;
+		bool is_pos;
+		if (rating <= Max_n) {
+			is_pos = false;
+		} else if (rating >= Min_p) {
+			is_pos = true;
+		} else {
+			cerr << "Unexpected Neutral: " << rating << "\n";
+			exit(0);
+		}
+
+		//classify the instance
+		if (classify(ss, use_bin)) {
+			if (is_pos) {
+				++tp;
+			} else {
+				++fp;
+			}
+		} else {
+			if (is_pos) {
+				++fn;
+			} else {
+				++tn;
+			}
+		}
+	}
+	in.close();
+
+	//print
+	cout << fixed << setprecision(6)
+	<< "Accuracy: " << (static_cast<double>(tp + tn) / (tp + tn + fp + fn)) * 100 << "%\n"
+	<< "Precision: " << (static_cast<double>(tp)) / (tp + fp) << "\n"
+	<< "Recall: " << (static_cast<double>(tp)) / (tp + fn) << "\n"
+	<< "F1 Measure: " << (2 * static_cast<double>(tp)) / (2 * tp + fp + fn) << "\n";
 }
