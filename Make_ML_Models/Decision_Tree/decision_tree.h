64888b590d844611e25edd0a423b32e26b446d4c
Lee_JunWon <cpprhtn@naver.com>
2020-09-24 23:19:05 +0900

    9/24

--- Make_ML_Models/Decision_Tree/decision_tree.h
+++ Make_ML_Models/Decision_Tree/decision_tree.h
@@ -1,3 +1,24 @@
 
 #include <string>
 #include <vector>
+
+class EX;
+
+class DecisionTree {
+	public:
+		void addAttrInfo(
+            const std::string& attr_name,
+			const std::vector<std::string>& attr_vals);
+
+		void addTargetValues(const std::vector<std::string>& target_values);
+
+		void build(const std::vector<EX>& train_data);
+
+        double test(const std::vector<EX>& test_data);
+
+		void prune(const std::vector<EX>& prune_data);
+
+		void print();
+
+		void printStats(const std::vector<EX>& test_data);
+};
