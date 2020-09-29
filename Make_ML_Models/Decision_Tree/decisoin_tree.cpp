10052ba949ec799261e34de414adfb1b431b6e19
Lee_JunWon <cpprhtn@naver.com>
2020-09-29 22:54:53 +0900

    9/29

---
+++ Make_ML_Models/Decision_Tree/decisoin_tree.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+
+#include "decision_tree.h"
+
+
+DecisionTreeNode::DecisionTreeNode() {
+	;
+}
+
+void DecisionTreeNode::setAttrName(const std::string& atb_name) {
+	this -> atb_name = atb_name;
+}
+
+std::string DecisionTreeNode::getAttrName() {
+	return atb_name;
+}
+
+void DecisionTreeNode::setType(const std::string& type) {
+	this -> type = type;
+}
+
+std::string DecisionTreeNode::getType() {
+	return type;
+}
+
+void DecisionTreeNode::setMaxTargetVal(const std::string& max_T_val) {
+	this -> max_T_val = max_T_val;
+}
+
+std::string DecisionTreeNode::getMaxTargetVal() {
+	return max_T_val;
